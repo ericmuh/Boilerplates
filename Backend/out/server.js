215bc2c7a0baae78c275df8b59657aa8e9b62545
ericmuh <muhwezi1000@gmail.com>
2022-09-09 17:18:50 +0300

    implement a simple api @ api/hello

--- Backend/out/server.js
+++ Backend/out/server.js
@@ -7,12 +7,24 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 require("dotenv").config();
+const languages = [
+    { language: "Spanish", greeting: "hola" },
+    { language: "French", greeting: "bonjour" },
+    { language: "German", greeting: "guten tag" },
+    { language: "Italian", greeting: "salve" },
+    { language: "Chinese", greeting: "nǐn hǎo" },
+    { language: "Portuguese", greeting: "olá" },
+    { language: "Arabic", greeting: "asalaam alaikum" },
+    { language: "Japanese", greeting: "konnichiwa" },
+    { language: "Korean", greeting: "anyoung haseyo" },
+    { language: "Russian", greeting: "Zdravstvuyte" },
+];
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
+app.get("/api/hello", (req, res) => {
+    res.json(languages);
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,15 +1,27 @@
 import express, { Express, Request, Response } from "express";
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import bodyParser from "body-parser";
+import cors from "cors";
 require("dotenv").config();
 
+const languages = [
+  { language: "Spanish", greeting: "hola" },
+  { language: "French", greeting: "bonjour" },
+  { language: "German", greeting: "guten tag" },
+  { language: "Italian", greeting: "salve" },
+  { language: "Chinese", greeting: "nǐn hǎo" },
+  { language: "Portuguese", greeting: "olá" },
+  { language: "Arabic", greeting: "asalaam alaikum" },
+  { language: "Japanese", greeting: "konnichiwa" },
+  { language: "Korean", greeting: "anyoung haseyo" },
+  { language: "Russian", greeting: "Zdravstvuyte" },
+];
 const app: Express = express();
 const port = process.env.PORT;
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json(languages);
 });
 
 app.listen(port, () => {
