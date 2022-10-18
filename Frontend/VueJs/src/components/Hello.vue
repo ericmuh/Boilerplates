bc1d86914f2aaef4f375d970310890cfc3a6f642
ericmuh <muhwezi1000@gmail.com>
2022-10-18 19:10:08 +0300

    📜set up intial setup for vue

--- Frontend/VueJs/src/components/Hello.vue
+++ Frontend/VueJs/src/components/Hello.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
+
+const greetings = {
+  language: "Luganda",
+  greeting: "Jebale",
+};
+
+const getHello = () => {
+  return;
+};
 </script>
 
 <template>
@@ -11,28 +20,23 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <div className="flex flex-col my-0 mx-auto enter align-center w-96 mt-48">
+      <h1 className="text-4xl text-center">
+        {{ greetings.language }} : {{ greetings.greeting }}
+      </h1>
+      <button
+        @oclick="
+          {
+            getHello;
+          }
+        "
+        className="bg-emerald-700  p-4 "
+      >
+        Click to say hello in different Languages.
+      </button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
