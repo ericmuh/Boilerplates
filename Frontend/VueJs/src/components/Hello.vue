6fcf4d847ce2c1506625fc935b16fab56038fc42
ericmuh <muhwezi1000@gmail.com>
2022-10-19 11:29:53 +0300

    ⤴️update methods file

--- Frontend/VueJs/src/components/Hello.vue
+++ Frontend/VueJs/src/components/Hello.vue
@@ -1,38 +1,40 @@
-<script setup lang="ts">
-import { ref } from "vue";
+<script lang="ts">
+import { getLanguages } from "../api";
 
-defineProps<{ msg: string }>();
-
-const count = ref(0);
-
-const greetings = {
-  language: "Luganda",
-  greeting: "Jebale",
-};
-
-const getHello = () => {
-  return;
+export default {
+  data() {
+    return {
+      question: "",
+      answer: "Questions usually contain a question mark. ;-)",
+      greetings: {
+        language: "Luganda",
+        greeting: "Jebale",
+      },
+    };
+  },
+  watch: {
+    // whenever question changes, this function will run
+    question(newQuestion, oldQuestion) {
+      if (newQuestion.includes("?")) {
+        this.getHello();
+      }
+    },
+  },
+  methods: {
+    async getHello() {
+      this.greetings = await getLanguages();
+    },
+  },
 };
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-
     <div className="flex flex-col my-0 mx-auto enter align-center w-96 mt-48">
       <h1 className="text-4xl text-center">
         {{ greetings.language }} : {{ greetings.greeting }}
       </h1>
-      <button
-        @oclick="
-          {
-            getHello;
-          }
-        "
-        className="bg-emerald-700  p-4 "
-      >
+      <button v-on:click="getHello" className="bg-emerald-700  p-4 ">
         Click to say hello in different Languages.
       </button>
     </div>
